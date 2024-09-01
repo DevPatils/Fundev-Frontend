0c3e76aa8b975856306e325734f972250cee7fe1
DevPatils <dev.patil1006@gmail.com>
2024-09-01 15:32:55 +0530

    funding work started

--- Fundevfrontend/src/App.tsx
+++ Fundevfrontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
+import Funding from './Pages/Funding';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/funding" element={<Funding/>} />
       </Routes>
     </Router>
   );
--- Fundevfrontend/src/Pages/Funding.tsx
+++ Fundevfrontend/src/Pages/Funding.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import TopNavbar from '../Components/TopNavbar'
+import BottomNavbar from '../Components/BottomNavbar'
+import Footer from '../Components/Footer'
+import OnboardForm from '../Components/startuponboarding'
 const Funding = () => {
   return (
-    <div>Funding</div>
+    <div>
+                <TopNavbar/>
+                <BottomNavbar/>
+                <OnboardForm onClose={() => {}}/>
+                <Footer/>
+    </div>
   )
 }
 
