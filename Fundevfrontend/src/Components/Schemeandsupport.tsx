aaf0c19bc332b9bd27b85e0be8b515c6999dda29
mandar4415 <mandarnikam9@gmail.com>
2024-09-02 14:34:35 +0530

    drive-link upload

--- Fundevfrontend/src/Components/Schemeandsupport.tsx
+++ Fundevfrontend/src/Components/Schemeandsupport.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
 
 const SchemeAndSupport = () => {
-  const navigate = useNavigate();
-
   return (
     <div className="p-6 bg-gray-100">
       <h1 className="font-extrabold text-2xl">Policies and Schemes</h1>
@@ -11,26 +8,37 @@ const SchemeAndSupport = () => {
       <table className="min-w-full bg-white border">
         <thead>
           <tr>
-            <th className="w-1/12 px-4 py-2 border text-left text-sm font-bold text-blue-900">ID</th>
-            <th className="w-5/12 px-4 py-2 border text-left text-sm font-bold text-blue-900">
-              DESCRIPTION OF RULES, REGULATIONS, ACTS INTRODUCED OR AMENDED TO SUPPORT STARTUPS
+            <th className="w-1/12 px-4 py-2 border text-left text-sm font-bold text-blue-900">
+              ID
             </th>
-            <th className="w-2/12 px-4 py-2 border text-left text-sm font-bold text-blue-900">DATE OF INTRODUCTION</th>
-            <th className="w-2/12 px-4 py-2 border text-left text-sm font-bold text-blue-900">FOR MORE DETAILS</th>
+            <th className="w-5/12 px-4 py-2 border text-left text-sm font-bold text-blue-900">
+              DESCRIPTION OF RULES, REGULATIONS, ACTS INTRODUCED OR AMENDED TO
+              SUPPORT STARTUPS
+            </th>
+            <th className="w-2/12 px-4 py-2 border text-left text-sm font-bold text-blue-900">
+              DATE OF INTRODUCTION
+            </th>
+            <th className="w-2/12 px-4 py-2 border text-left text-sm font-bold text-blue-900">
+              FOR MORE DETAILS
+            </th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td className="px-4 py-2 border text-sm font-semibold text-gray-700">1.</td>
-            <td className="px-4 py-2 border text-base text-gray-700">Angel Investors Facilitation</td>
-            <td className="px-4 py-2 border text-base text-gray-700">Apr 25, 2018</td>
+            <td className="px-4 py-2 border text-sm font-semibold text-gray-700">
+              1.
+            </td>
+            <td className="px-4 py-2 border text-base text-gray-700">
+              Angel Investors Facilitation
+            </td>
+            <td className="px-4 py-2 border text-base text-gray-700">
+              Apr 25, 2018
+            </td>
             <td className="px-4 py-2 border text-center">
               <a
-                href="#"
-                onClick={(e) => {
-                  e.preventDefault();
-                  navigate('/your-url');
-                }}
+                href="https://drive.google.com/file/d/1Rd4T3NHbNIkeCt-NQwOpb_6ml17DbL4Y/view?usp=drive_link" // Pocicy_1
+                target="_blank" // Open link in a new tab
+                rel="noopener noreferrer" // Security reasons for opening links
                 className="bg-orange-500 hover:bg-orange-600 text-white font-semibold py-1 px-4 rounded inline-block text-center"
               >
                 View
@@ -38,16 +46,20 @@ const SchemeAndSupport = () => {
             </td>
           </tr>
           <tr>
-            <td className="px-4 py-2 border text-sm font-semibold text-gray-700">2.</td>
-            <td className="px-4 py-2 border text-base text-gray-700">Science, Technology and Innovation policy of Gujarat</td>
-            <td className="px-4 py-2 border text-base text-gray-700">Apr 15, 2018</td>
+            <td className="px-4 py-2 border text-sm font-semibold text-gray-700">
+              2.
+            </td>
+            <td className="px-4 py-2 border text-base text-gray-700">
+              Science, Technology and Innovation policy of Gujarat
+            </td>
+            <td className="px-4 py-2 border text-base text-gray-700">
+              Apr 15, 2018
+            </td>
             <td className="px-4 py-2 border text-center">
               <a
-                href="#"
-                onClick={(e) => {
-                  e.preventDefault();
-                  navigate('/your-url'); // Replace '/your-url' with the actual URL
-                }}
+                href="https://drive.google.com/file/d/1K57dKVuM160pKdZNW-mQsWmr6IkLFUuW/view?usp=drive_link" // Policy_2
+                target="_blank"
+                rel="noopener noreferrer"
                 className="bg-orange-500 hover:bg-orange-600 text-white font-semibold py-1 px-4 rounded inline-block text-center"
               >
                 View
@@ -55,16 +67,20 @@ const SchemeAndSupport = () => {
             </td>
           </tr>
           <tr>
-            <td className="px-4 py-2 border text-sm font-semibold text-gray-700">3.</td>
-            <td className="px-4 py-2 border text-base text-gray-700">Exemptions - MSMEs & Start Ups in purchases made by the Government</td>
-            <td className="px-4 py-2 border text-base text-gray-700">Apr 11, 2018</td>
+            <td className="px-4 py-2 border text-sm font-semibold text-gray-700">
+              3.
+            </td>
+            <td className="px-4 py-2 border text-base text-gray-700">
+              Exemptions - MSMEs & Start Ups in purchases made by the Government
+            </td>
+            <td className="px-4 py-2 border text-base text-gray-700">
+              Apr 11, 2018
+            </td>
             <td className="px-4 py-2 border text-center">
               <a
-                href="#"
-                onClick={(e) => {
-                  e.preventDefault();
-                  navigate('/your-url'); // Replace '/your-url' with the actual URL
-                }}
+                href="https://drive.google.com/file/d/14dYJksG5g0O5ALNShl7LI7tphvbEb6UQ/view?usp=drive_link" // Policy_3
+                target="_blank"
+                rel="noopener noreferrer"
                 className="bg-orange-500 hover:bg-orange-600 text-white font-semibold py-1 px-4 rounded inline-block text-center"
               >
                 View
@@ -72,16 +88,21 @@ const SchemeAndSupport = () => {
             </td>
           </tr>
           <tr>
-            <td className="px-4 py-2 border text-sm font-semibold text-gray-700">4.</td>
-            <td className="px-4 py-2 border text-base text-gray-700">Amendment in Self-Certification-cum-Consolidated Annual Return Scheme</td>
-            <td className="px-4 py-2 border text-base text-gray-700">Mar 22, 2018</td>
+            <td className="px-4 py-2 border text-sm font-semibold text-gray-700">
+              4.
+            </td>
+            <td className="px-4 py-2 border text-base text-gray-700">
+              Amendment in Self-Certification-cum-Consolidated Annual Return
+              Scheme
+            </td>
+            <td className="px-4 py-2 border text-base text-gray-700">
+              Mar 22, 2018
+            </td>
             <td className="px-4 py-2 border text-center">
               <a
-                href="#"
-                onClick={(e) => {
-                  e.preventDefault();
-                  navigate('/your-url'); // Replace '/your-url' with the actual URL
-                }}
+                href="https://drive.google.com/file/d/1Yco0LDmcvSc9kLPyGABQOuosh-M53148/view?usp=drive_link" // Policy_4
+                target="_blank"
+                rel="noopener noreferrer"
                 className="bg-orange-500 hover:bg-orange-600 text-white font-semibold py-1 px-4 rounded inline-block text-center"
               >
                 View
@@ -89,16 +110,21 @@ const SchemeAndSupport = () => {
             </td>
           </tr>
           <tr>
-            <td className="px-4 py-2 border text-sm font-semibold text-gray-700">5.</td>
-            <td className="px-4 py-2 border text-base text-gray-700">Amendment in Self-Certification-cum-Consolidated Annual Return Scheme</td>
-            <td className="px-4 py-2 border text-base text-gray-700">Mar 9, 2017</td>
+            <td className="px-4 py-2 border text-sm font-semibold text-gray-700">
+              5.
+            </td>
+            <td className="px-4 py-2 border text-base text-gray-700">
+              Amendment in Self-Certification- cum-Consol idated Annual Return
+              Scheme under the Labour Laws
+            </td>
+            <td className="px-4 py-2 border text-base text-gray-700">
+              Mar 9, 2017
+            </td>
             <td className="px-4 py-2 border text-center">
               <a
-                href="#"
-                onClick={(e) => {
-                  e.preventDefault();
-                  navigate('/your-url'); // Replace '/your-url' with the actual URL
-                }}
+                href="https://drive.google.com/file/d/1jHZlVlaUVLH3RbIKBGxSmsc7ivxEydaJ/view?usp=drive_link" // Policy_5
+                target="_blank"
+                rel="noopener noreferrer"
                 className="bg-orange-500 hover:bg-orange-600 text-white font-semibold py-1 px-4 rounded inline-block text-center"
               >
                 View
@@ -106,16 +132,20 @@ const SchemeAndSupport = () => {
             </td>
           </tr>
           <tr>
-            <td className="px-4 py-2 border text-sm font-semibold text-gray-700">6.</td>
-            <td className="px-4 py-2 border text-base text-gray-700">Self-Certification-cum-Consolidated Annual Return Scheme</td>
-            <td className="px-4 py-2 border text-base text-gray-700">Dec 31, 2016</td>
+            <td className="px-4 py-2 border text-sm font-semibold text-gray-700">
+              6.
+            </td>
+            <td className="px-4 py-2 border text-base text-gray-700">
+              Self-Certification-cum-Consolidated Annual Return Scheme
+            </td>
+            <td className="px-4 py-2 border text-base text-gray-700">
+              Dec 31, 2016
+            </td>
             <td className="px-4 py-2 border text-center">
               <a
-                href="#"
-                onClick={(e) => {
-                  e.preventDefault();
-                  navigate('/your-url'); // Replace '/your-url' with the actual URL
-                }}
+                href="https://drive.google.com/file/d/1S7P-HF9fmHtALFzBP12ARx7Svdg99gee/view?usp=drive_link" // Policy_6
+                target="_blank"
+                rel="noopener noreferrer"
                 className="bg-orange-500 hover:bg-orange-600 text-white font-semibold py-1 px-4 rounded inline-block text-center"
               >
                 View
